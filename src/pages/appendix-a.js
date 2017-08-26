adc2cab2c9abb6d82173ecba554be4f53fa8adcf
Kiran Makkapati <chandmk@gmail.com>
2017-08-26 08:36:24 -0400

    fixed spelling

--- src/pages/appendix-a.js
+++ src/pages/appendix-a.js
@@ -13,9 +13,9 @@ class AppendixA extends Component {
     constructor() {
         super()
         this.state = { title: '', 
-                        statue: '', 
+                        statute: '', 
                         guidelines:[],
-                        statueOptionList:null }
+                        statuteOptionList:null }
     }
    
     render() {
@@ -31,16 +31,16 @@ class AppendixA extends Component {
 	    <p>This index specifies the offense guideline section(s) in Chapter Two (Offense Conduct) applicable to the statute of conviction. If more than one guideline section is referenced for the particular statute, use the guideline most appropriate for the offense conduct charged in the count of which the defendant was convicted. For the rules governing the determination of the offense guideline section(s) from Chapter Two, and for any exceptions to those rules, see §1B1.2 (Applicable Guidelines). </p>
         <section>
             <label htmlFor="title">Title</label>
-            <select id="title" onChange={this.cascadeStatue.bind(this)}
+            <select id="title" onChange={this.cascadestatute.bind(this)}
                 value={this.state.title}>
                 <option>Select</option>
                 {titleOptionList}
             </select>
-            <label htmlFor="statue">Statue</label>
-            <select id="statue" value={this.state.statue} 
-                onChange={e => this.setState({statue: e.target.value})}>
+            <label htmlFor="statute">Statute</label>
+            <select id="statute" value={this.state.statute} 
+                onChange={e => this.setState({statute: e.target.value})}>
                 <option>Select</option>
-                {this.state.statueOptionList}
+                {this.state.statuteOptionList}
             </select>
             <button onClick={this.search.bind(this)}>Go</button>
             </section>
@@ -48,17 +48,17 @@ class AppendixA extends Component {
         </Container>);
     }
 
-    cascadeStatue(e)  { 
+    cascadestatute(e)  { 
         let title = e.target.value;
-        let statueOptions = data.filter(d => d.Title == title)
-                        .map(x => <option key={x.Statue}>{x.Statue}</option>);
-        this.setState({title: title, statueOptionList: statueOptions}) ;
+        let statuteOptions = data.filter(d => d.Title == title)
+                        .map(x => <option key={x.Statute}>{x.Statute}</option>);
+        this.setState({title: title, statuteOptionList: statuteOptions}) ;
     }
 
     search() {
         
         let results =  data.find(d => d.Title == this.state.title &&
-            d.Statue == this.state.statue);
+            d.Statute == this.state.statute);
         if(results == undefined) return;
        
         let guidelines = results.Guidelines.split(",")
