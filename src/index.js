a20813c2f1212bce4476d6740fa4489e844f8587
chandmk <chandmk@gmail.com>
2017-09-08 09:55:11 -0400

    added chunks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
-import { render } from 'react-snapshot';
+import ReactDOM from 'react-dom';
+//import { render } from 'react-snapshot';
 import './index.css';
 import 'uswds/dist/css/uswds.css';
 import 'uswds';
@@ -8,8 +8,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import registerAppCache from './registerAppCache';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+//render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
 registerAppCache();
