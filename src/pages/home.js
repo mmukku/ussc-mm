63fd6299be9ebe2e844b8f0f147b39081e34b6bd
Kiran Makkapati <chandmk@gmail.com>
2017-10-16 21:41:12 -0400

    force load appcache

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import chapters from '../data/chapters.json';
+import parts from '../data/parts.json';
 import PartsLink from '../components/partsLink';
+import GuideLines from '../data/guidelines.json';
+import sections from '../data/sections.json';
+import Data from '../data/gl.json';
 
 const chapterList = chapters.map(c => (
   <p key={c.id}>
@@ -9,7 +13,10 @@ const chapterList = chapters.map(c => (
     </PartsLink>
   </p>
 ));
-
+const lp = parts.length;
+const lg = GuideLines.length;
+const ls = sections.length;
+const ld = Data.length;
 export default () => (
   <div>
     <h3>Guidelines</h3>
