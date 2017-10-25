0a1e5cf5a899e317a865c47845df3ea8eb99efe0
William Haddon <whaddon@swingtech.com>
2017-10-25 10:38:12 -0400

    Revert "Custom service worker has client-initiated data storage capabilities"
    This reverts commit 76a6251597415ce3ffe962bf8f3fd805e0fb06f2.

--- public/ussc-service-worker.js
+++ public/ussc-service-worker.js
@@ -1,50 +1,16 @@
 onfetch = function(event) {
-	var path, origin;
-	path = event.request.url.split('/').slice(3).join('/');
-	origin = event.request.url.split('/').slice(0, 3).join('/');
-	if (path.indexOf('sw_read/') === 0) {
-		/* special actions to read custom data -- only read from cache -- can not be loaded from the
-			internet */
-		event.respondWith(
-			caches.open('ussc').then(function(cache) {
-				return cache.match(event.request).then(function(response) {
-					if (response) {
+	event.respondWith(
+		caches.open('ussc').then(function(cache) {
+			return cache.match(event.request).then(function(response) {
+				if (response) {
+					return response;
+				} else {
+					return fetch(event.request).then(function(response) {
+						cache.put(event.request, response.clone());
 						return response;
-					} else {
-						return new Response('Not found', {status: '404'});
-					}
-				});
-			})
-		);
-	} else if (path.indexOf('sw_write/') === 0) {
-		/* special actions to write custom data */
-		event.respondWith(
-			caches.open('ussc').then(function(cache) {
-				parts = path.split('/');
-				if (parts.length < 3) {
-					return new Response('Not found', {status: '404'});
+					});
 				}
-				key = parts[1];
-				value = parts[2];
-				cache.put(origin + '/sw_read/' + key, new Response(value, {status: '200'}));
-				return new Response('Success', {status: '200'});
-			})
-		);
-	} else {
-		/* respond to ordinary URLs like an ordinary cache */
-		event.respondWith(
-			caches.open('ussc').then(function(cache) {
-				return cache.match(event.request).then(function(response) {
-					if (response) {
-						return response;
-					} else {
-						return fetch(event.request).then(function(response) {
-							cache.put(event.request, response.clone());
-							return response;
-						});
-					}
-				});
-			})
-		);
-	}
+			});
+		})
+	);
 };
