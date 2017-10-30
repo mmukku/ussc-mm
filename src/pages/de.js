c61898956226d0fcaa1fa723337c481a1be056e8
Kiran Makkapati <chandmk@gmail.com>
2017-10-30 19:45:36 -0400

    issue #42

--- src/pages/de.js
+++ src/pages/de.js
@@ -9,33 +9,66 @@ const conversionTable = [
   { uom: 'gal', factor: 3.785, targetUOM: 'liters' },
   { uom: 'qt', factor: 0.946, targetUOM: 'liters' },
   { uom: 'gm', factor: 1, targetUOM: 'ml' },
+  { uom: 'ml', factor: 1, targetUOM: 'gm' },
   { uom: 'liter', factor: 1000, targetUOM: 'ml' },
+  { uom: 'mg', factor: 0.001, targetUOM: 'gm' },
   { uom: 'kg', factor: 1000, targetUOM: 'gm' },
   { uom: 'gm', factor: 1000, targetUOM: 'mg' },
   { uom: 'grain', factor: 64.8, targetUOM: 'mg' }
 ];
 
-const weightPerDoseTable = [
-  { substance: 'MDA', targetWeight: 250, targetUOM: 'mg' },
-  { substance: 'MDMA', targetWeight: 250, targetUOM: 'mg' },
-  { substance: 'Mescaline', targetWeight: 500, targetUOM: 'mg' },
-  { substance: 'PCP*', targetWeight: 5, targetUOM: 'mg' },
-  { substance: 'Peyote (dry)', targetWeight: 12, targetUOM: 'gm' },
-  { substance: 'Peyote (wet)', targetWeight: 120, targetUOM: 'gm' },
-  { substance: 'Psilocin*', targetWeight: 10, targetUOM: 'mg' },
-  { substance: 'Psilocybe mushrooms (dry)', targetWeight: 5, targetUOM: 'gm' },
-  { substance: 'Psilocybe mushrooms (wet)', targetWeight: 50, targetUOM: 'gm' },
-  { substance: 'Psilocybin*', targetWeight: 10, targetUOM: 'mg' },
+const Notes =
+  'Provided, that the minimum offense level from the Drug Quantity Table for this controlled substance individually, or in combination with another controlled substance, is level 12.';
+
+const NotesTable = [
   {
-    substance: '2,5-Dimethoxy-4-methylamphetamine (STP, DOM)*',
-    targetWeight: 3,
-    targetUOM: 'mg'
+    substances: 'Flunitrazepam',
+    notes:
+      'Provided, that the minimum offense level from the Drug Quantity Table for flunitrazepam individually, or in combination with any Schedule I or II depressants, Schedule III substances, Schedule IV substances, and Schedule V substances is level 8.'
   },
-  { substance: 'marihuana cigarette', targetWeight: 0.5, targetUOM: 'gm' },
-  { substance: 'Amphetamine*', targetWeight: 10, targetUOM: 'mg' },
-  { substance: 'Methamphetamine*', targetWeight: 5, targetUOM: 'mg' },
-  { substance: 'Phenmetrazine (Preludin)*', targetWeight: 75, targetUOM: 'mg' }
+  {
+    substances: 'Schedule III Substances (except ketamine)',
+    notes:
+      'Provided, that the combined equivalent weight of all Schedule III substances (except ketamine), Schedule IV substances (except flunitrazepam), and Schedule V substances shall not exceed 79.99 kilograms of marihuana.'
+  },
+  {
+    substances: 'Schedule IV Substances (except Flunitrazepam)',
+    notes:
+      'Provided, that the combined equivalent weight of all Schedule IV (except flunitrazepam) and V substances shall not exceed 9.99 kilograms of marihuana.'
+  },
+  {
+    substances: 'Schedule V Substances',
+    notes:
+      'Provided, that the combined equivalent weight of Schedule V substances shall not exceed 2.49 kilograms of marihuana.'
+  },
+  {
+    substances: 'Ephedrine,Phenylpropanolamine,Pseudoephedrine',
+    notes:
+      'Provided, that in a case involving ephedrine, pseudoephedrine, or phenylpropanolamine tablets, use the weight of the ephedrine, pseudoephedrine, or phenylpropanolamine contained in the tablets, not the weight of the entire tablets, in calculating the base offense level.'
+  }
 ];
+
+// const weightPerDoseTable = [
+//   { substance: 'MDA', targetWeight: 250, targetUOM: 'mg' },
+//   { substance: 'MDMA', targetWeight: 250, targetUOM: 'mg' },
+//   { substance: 'Mescaline', targetWeight: 500, targetUOM: 'mg' },
+//   { substance: 'PCP*', targetWeight: 5, targetUOM: 'mg' },
+//   { substance: 'Peyote (dry)', targetWeight: 12, targetUOM: 'gm' },
+//   { substance: 'Peyote (wet)', targetWeight: 120, targetUOM: 'gm' },
+//   { substance: 'Psilocin*', targetWeight: 10, targetUOM: 'mg' },
+//   { substance: 'Psilocybe mushrooms (dry)', targetWeight: 5, targetUOM: 'gm' },
+//   { substance: 'Psilocybe mushrooms (wet)', targetWeight: 50, targetUOM: 'gm' },
+//   { substance: 'Psilocybin*', targetWeight: 10, targetUOM: 'mg' },
+//   {
+//     substance: '2,5-Dimethoxy-4-methylamphetamine (STP, DOM)*',
+//     targetWeight: 3,
+//     targetUOM: 'mg'
+//   },
+//   { substance: 'marihuana cigarette', targetWeight: 0.5, targetUOM: 'gm' },
+//   { substance: 'Amphetamine*', targetWeight: 10, targetUOM: 'mg' },
+//   { substance: 'Methamphetamine*', targetWeight: 5, targetUOM: 'mg' },
+//   { substance: 'Phenmetrazine (Preludin)*', targetWeight: 75, targetUOM: 'mg' }
+// ];
 
 let substanceList = _.uniqBy(data, 'substance');
 
@@ -72,7 +105,14 @@ class DE extends Component {
       if (conversion !== undefined) {
         conversionFactor = conversion.factor;
       }
-      // console.log(this.state.uom, sourceUOM, conversionFactor);
+      let notes = Notes;
+      let noteEntry = _.find(
+        NotesTable,
+        nt => nt.substances.indexOf(this.state.substance) >= 0
+      );
+      if (noteEntry !== undefined) {
+        notes = noteEntry.notes;
+      }
       result = (
         <section>
           <div className="usa-alert usa-alert-info">
@@ -84,6 +124,7 @@ class DE extends Component {
                   this.state.marihuana.targetWeight}{' '}
                 {this.state.marihuana.targetUOM}
               </h4>
+              <em>{notes}</em>
             </div>
           </div>
         </section>
@@ -92,7 +133,15 @@ class DE extends Component {
     return (
       <div>
         <h2>Drug Equivalency Calculator</h2>
+        <p>
+          Use the Drug Equivalency Calculator to convert the quantity of
+          controlled substance involved in the offense to its equivalent
+          quantity of marihuana. In a case involving more than one controlled
+          substance, convert each controlled substance into its marihuana
+          equivalency and then add all converted quantities.
+        </p>
         {result}
+
         <section>
           <label htmlFor="substance">Substance</label>
           <select
@@ -113,8 +162,6 @@ class DE extends Component {
             id="uom"
             onChange={e => this.setState({ uom: e.target.value })}
           >
-            <option>Select</option>
-            <option value="gm">gm</option>
             {this.state.uomList}
           </select>
           <button onClick={this.calculate.bind(this)}>Go</button>
@@ -124,14 +171,21 @@ class DE extends Component {
   }
 
   getUOMList(e) {
-    let uom = _.filter(data, d => d.substance === e.target.value)[0].sourceUOM;
+    let uom = _.filter(data, d => d.substance === e.target.value)[0]
+      .substanceUOM;
     if (uom === undefined) {
       uom = 'gm';
     }
-    var uoml = _.filter(
-      conversionTable,
-      d => d.targetUOM === uom || d.sourceUOM === uom
-    ).map(x => x.uom);
+    let uoml = [];
+    uoml[0] = uom;
+
+    uoml = _.concat(
+      uoml,
+      _.filter(
+        conversionTable,
+        d => d.sourceUOM === uom || d.targetUOM === uom
+      ).map(x => x.uom)
+    );
 
     let uomList = uoml.map(x => <option key={x}>{x}</option>);
 
