b9d4edc1e5bf6734c5626de2de25aa30ab1b4943
Kiran Makkapati <chandmk@gmail.com>
2017-11-05 19:26:22 -0500

    added support for addressable link content

--- src/pages/appendixbpart.js
+++ src/pages/appendixbpart.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import data from '../data/appendix-b';
+import scrollToElment from 'scroll-to-element';
 import _ from 'lodash';
 
-export default props => {
-  let content = _.find(data, b => b.id === props.match.params.part).content;
-  return (
-    <div>
-      <a href="/ab">AppendixB</a>
-      <p dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
-  );
-};
+class AppendixBPart extends React.Component {
+  componentDidMount() {
+    let hash = this.props.history.location.hash;
+    if (hash) {
+      scrollToElment(hash, { ease: null, duration: 0 });
+    }
+  }
+  render() {
+    let content = _.find(data, b => b.id === this.props.match.params.part)
+      .content;
+    return (
+      <div>
+        <a href="/ab">AppendixB</a>
+        <p dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    );
+  }
+}
+
+export default AppendixBPart;
