c758edf6dea8a238905bd612a79eed8e735cdcf1
William Haddon <whaddon@swingtech.com>
2017-11-15 13:31:31 -0500

    Don't allow duplicate bookmarks.

--- src/bookmark.js
+++ src/bookmark.js
@@ -1,17 +1,31 @@
-import { get_list, set_list, remove_list_item, add_list_item } from './locallist';
+import {
+  get_list,
+  set_list,
+  remove_list_item,
+  add_list_item
+} from './locallist';
 
 export function get_bookmarks() {
-  return get_list(JSON.stringify({type: 'ussc-bookmarks'}));
+  return get_list(JSON.stringify({ type: 'ussc-bookmarks' }));
 }
 
 export function set_bookmarks(bookmarks) {
-  set_list(JSON.stringify({type: 'ussc-bookmarks'}));
+  set_list(JSON.stringify({ type: 'ussc-bookmarks' }));
 }
 
 export function remove_bookmark(bookmark_id) {
-  remove_list_item(JSON.stringify({type: 'ussc-bookmarks'}), bookmark_id);
+  remove_list_item(JSON.stringify({ type: 'ussc-bookmarks' }), bookmark_id);
 }
 
 export function add_bookmark(path, title) {
-  add_list_item(JSON.stringify({type: 'ussc-bookmarks'}), {path: path, title: title});
+  let bookmarks = get_bookmarks();
+  for (var i = 0; i < bookmarks.data.length; i++) {
+    if (bookmarks.data[i].path === path && bookmarks.data[i].title === title) {
+      return;
+    }
+  }
+  add_list_item(JSON.stringify({ type: 'ussc-bookmarks' }), {
+    path: path,
+    title: title
+  });
 }
